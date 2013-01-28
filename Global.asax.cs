55facef985a9318417b8746cbfec572b8316c3ef
Suraj Bhagavan <suri1988@gmail.com>
2013-01-28 14:30:33 -0500

    committing

--- Global.asax.cs
+++ Global.asax.cs
@@ -95,18 +95,5 @@ namespace ServiceStack.Hello
         }
     }
 
-    public class AuthTest
-    {
-        const string BaseUrl = "https://localhost:62577/ServiceStack.Hello";
-        public static void Main(string[] args)
-        {
-            //adding test comment
-            //adding test commen2
-            var restClient = new JsonServiceClient(BaseUrl);
-            restClient.SetCredentials("john", "test");
-            restClient.AlwaysSendBasicAuthHeader = true;
-            HelloResponse response = restClient.Get<HelloResponse>("/hello/Suraj");
-            Console.WriteLine(response.Result);
-        }
-    }
+   
 }
